f87c50dae3475f09c87229343fb95b8fa29ecd4c
SGM <sergio.garcia52@alu.uclm.es>
2019-12-16 19:04:10 +0100

    wosel el miercoles vuelca ssoo

--- src/presentacion/Callejero.java
+++ src/presentacion/Callejero.java
@@ -64,6 +64,7 @@ public class Callejero {
 	 */
 	private void initialize() {
 		frame = new JFrame();
+		frame.setResizable(false);
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
--- src/presentacion/Login2.java
+++ src/presentacion/Login2.java
@@ -56,6 +56,7 @@ public class Login2 {
 	 */
 	private void initialize() {
 		frame = new JFrame();
+		frame.setResizable(false);
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
